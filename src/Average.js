2d93885b0b982b1253b9a0c50b2d2fe9a9ac329c
seilylook@naver.com <seilylook@naver.com>
2021-03-15 15:00:13 +0900

    hooks_useref

--- src/Average.js
+++ src/Average.js
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from "react";
+import React, { useState, useMemo, useCallback, useRef } from "react";
 
 const getAverage = numbers => {
   console.log("평균값 계산 중...");
@@ -12,15 +12,17 @@ const getAverage = numbers => {
 const Average = () => {
   const [list, setList] = useState([]);
   const [number, setNumber] = useState("");
+  const inputEl = useRef(null);
 
-  const onChange = e => {
+  const onChange = useCallback(e => {
     setNumber(e.target.value);
-  };
-  const onInsert = e => {
+  }, []);
+  const onInsert = useCallback(() => {
     const nextList = list.concat(parseInt(number));
     setList(nextList);
     setNumber("");
-  };
+    inputEl.current.focus();
+  }, [number, list]);
 
   const avg = useMemo(() => getAverage(list), [list]);
 
